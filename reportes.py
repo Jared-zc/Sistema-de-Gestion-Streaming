49f27acf1e1809297c832c156fbfa40dfb1628ed
Jared-zc <oajared69@gmail.com>
2026-05-23 16:41:10 -0500

    Delete reportes.py

--- reportes.py
+++
@@ -1,7 +0,0 @@
-def reporte_general(usuarios, contenidos, suscripciones, reproducciones):
-    return {
-        "total_usuarios": len(usuarios),
-        "total_contenidos": len(contenidos),
-        "total_suscripciones": len(suscripciones),
-        "total_reproducciones": len(reproducciones)
-    }
